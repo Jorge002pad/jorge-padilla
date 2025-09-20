919e12406c7e988046784a08bd15995d90c0e976
kansas <jorgepad002@gmail.com>
2025-09-19 21:55:25 -0600

    Refactor App component to use Bootstrap and include Header component, removing initial Vite and React logos and state management.

---
+++ jorge-padilla/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header: React.FC = () => {
+  return (
+    <Navbar bg="light" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">JORGE PADILLA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#about">Sobre Mí</Nav.Link>
+            <Nav.Link href="#skills">Habilidades</Nav.Link>
+            <Nav.Link href="#experience">Experiencia</Nav.Link>
+            <Nav.Link href="#contact">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
